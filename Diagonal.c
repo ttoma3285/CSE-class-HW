0985aaa29387eb78b09da7c0247f1a7f257d78d7
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-16 01:52:02 +0600

    Add program to calculate diagonal of a square

---
+++ Diagonal.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+#include <math.h>
+
+int main() {
+   float a,diagonal;
+    printf("Enter the value of a:");
+    scanf("%f",&a);
+    diagonal=a* sqrt(2);
+    printf("diagonal value = %.2f",diagonal);
+}
