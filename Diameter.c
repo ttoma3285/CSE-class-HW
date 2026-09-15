9a244547f241f7f09ad4bd39ca68e2add08e5218
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-16 01:52:39 +0600

    Add Diameter.c to calculate diameter from radius

---
+++ Diameter.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+
+int main() {
+    // Write C code here
+   float r,diameter;
+    printf("Enter the value of r:");
+    scanf("%f",&r);
+    diameter=2*r;
+    printf("diameter value=%.2f",diameter);
+}
